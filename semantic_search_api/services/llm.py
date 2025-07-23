2de89603c7316a3d3d7b21778e396e36e7dbad8d
David <d0961150216d@gmail.com>
2025-07-23 11:01:44 +0800

    add llm.py

---
+++ semantic_search_api/services/llm.py
@@ -0,0 +1,23 @@
+import logging
+import google.generativeai as genai
+import os
+from semantic_search_api.promtps.summarize_news_prompt import SUMMARIZE_NEWS_PROMPT
+from typing import List, Dict
+
+logger = logging.getLogge(__name__)
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel("gemini-2.5-pro")
+
+def build_prompt(query: str, results: List[Dict]) -> str:
+    return SUMMARIZE_NEWS_PROMPT.format(
+        query=query,
+        results=results
+    )
+
+def call_gemini(prompt: str) -> str:
+    response = model.generate_content(prompt)
+    
+    return response.text
+
